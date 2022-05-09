693df2539050e64d1406844749a4f8971c6bf144
Gabriel Muquinho <gmuquinho@gmail.com>
2022-05-09 18:28:15 -0300

    Added click functions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,3 +4,27 @@ let numbers = document.querySelector('.division-1-text-3');
 let desc = document.querySelector('.division-1-text-4 p');
 let tutorial = document.querySelector('.division-2');
 let side = document.querySelector('.division-1-right');
+
+
+// ============================================
+// Creating the functions for the buttons
+// ============================================
+
+function clicked(n){
+    alert(`Pressionou o botão ${n}!`);
+};
+
+function white(){
+    alert("Pressionou o botão branco!");
+};
+
+function correct(){
+    alert("Pressionou o botão corrige!");
+};
+
+function confirm(){
+    alert("Pressionou o botão confirma!");
+};
+
+// ============================================
+// ============================================
