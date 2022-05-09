dcfe072b17414d07df1113421d1740d763595099
Gabriel Muquinho <gmuquinho@gmail.com>
2022-05-09 18:09:18 -0300

    creation of new scripts and array of votes. Images also added.

---
+++ assets/scripts/script.js
@@ -0,0 +1,6 @@
+let substituteText = document.querySelector('.division-1-text-1 p');
+let politicalOffice = document.querySelector('.division-1-text-2 p');
+let numbers = document.querySelector('.division-1-text-3');
+let desc = document.querySelector('.division-1-text-4 p');
+let tutorial = document.querySelector('.division-2');
+let side = document.querySelector('.division-1-right');
---
+++ assets/scripts/stages.js
@@ -0,0 +1,45 @@
+let stages = [
+    {
+        title: "PRESIDENTE",
+        numbers: 5,
+        candidates: [
+            {
+                number: 78000,
+                name: "GOKU",
+                political_party: DBZ,
+                photo: [
+                    {url: "goku.png", legend: "PRESIDENTE", small: false}
+                ],
+
+                number: 96455,
+                name: "LUFFY",
+                political_party: OP,
+                photo: [
+                    {url: "luffy.png", legend: "PRESIDENTE"}
+                ]
+            },
+        ]
+    },
+    
+    {
+        title: "GOVERNADOR",
+        numbers: 5,
+        candidates: [
+            {
+                number: 78001,
+                name: "KURIRIN",
+                political_party: DBZ,
+                photo: [
+                    {url: "kuririn.png", legend: "GOVERNADOR", small: true}
+                ],
+
+                number: 96456,
+                name: "ZORO",
+                political_party: OP,
+                photo: [
+                    {url: "zoro.png", legend: "GOVERNADOR"}
+                ]
+            }
+        ]
+    }
+];
