c8c21bd4287699963a139436f78437a4ecd11258
Gabriel Muquinho <gmuquinho@gmail.com>
2022-05-09 21:42:13 -0300

    Update scripts

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,42 @@
-let substituteText = document.querySelector('.division-1-text-1 p');
-let politicalOffice = document.querySelector('.division-1-text-2 p');
-let numbers = document.querySelector('.division-1-text-3');
-let desc = document.querySelector('.division-1-text-4 p');
+let substituteText = document.querySelector('.division-1-text-1 span');
+let politicalOffice = document.querySelector('.division-1-text-2 span');
+let desc = document.querySelector('.division-1-text-4');
 let tutorial = document.querySelector('.division-2');
 let side = document.querySelector('.division-1-right');
+let numbers = document.querySelector('.division-1-text-3');
+
+// ============================================
+
+let atualStage = 0;
+let number = '';
+
+
+function startStage(){
+    let stage = stages[atualStage];
+
+    let numberHTML = '';
+
+    for(let i=0; i < stage.numbers; i++){
+        if(i == 0){
+            numberHTML += '<div class="number-box blink-effect"></div>';
+        } else {
+            numberHTML += '<div class="number-box"></div>';
+        }
+    };
+
+
+    substituteText.style.display = 'none';
+    politicalOffice.innerHTML = stage.title;
+    desc.innerHTML = '';
+    tutorial.style.display = 'none';
+    side.innerHTML = '';
+    numbers.innerHTML = numberHTML;
+};
+
+
+function interfaceUpdate(){
+
+};
 
 
 // ============================================
@@ -11,7 +44,11 @@ let side = document.querySelector('.division-1-right');
 // ============================================
 
 function clicked(n){
-    alert(`Pressionou o botão ${n}!`);
+    let vote_number = document.querySelector('.number-box .blink-effect');
+    if(vote_number !== null){
+        vote_number.innerHTML = n;
+        number = `${number}${n}`;
+    };
 };
 
 function white(){
@@ -28,3 +65,6 @@ function confirm(){
 
 // ============================================
 // ============================================
+
+
+startStage();
--- assets/scripts/stages.js
+++ assets/scripts/stages.js
@@ -6,14 +6,14 @@ let stages = [
             {
                 number: 78000,
                 name: "GOKU",
-                political_party: DBZ,
+                political_party: 'DBZ',
                 photo: [
                     {url: "goku.png", legend: "PRESIDENTE", small: false}
                 ],
 
                 number: 96455,
                 name: "LUFFY",
-                political_party: OP,
+                political_party: 'OP',
                 photo: [
                     {url: "luffy.png", legend: "PRESIDENTE"}
                 ]
@@ -28,14 +28,14 @@ let stages = [
             {
                 number: 78001,
                 name: "KURIRIN",
-                political_party: DBZ,
+                political_party: 'DBZ',
                 photo: [
                     {url: "kuririn.png", legend: "GOVERNADOR", small: true}
                 ],
 
                 number: 96456,
                 name: "ZORO",
-                political_party: OP,
+                political_party: 'OP',
                 photo: [
                     {url: "zoro.png", legend: "GOVERNADOR"}
                 ]
